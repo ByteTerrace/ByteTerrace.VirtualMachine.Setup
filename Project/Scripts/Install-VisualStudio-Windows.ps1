0de2178af374a79c9e5ac1208d9efca06e77fb20
David Smith <kittoes0124@byteterrace.com>
2022-06-08 23:40:17 -0500

    refactored component handling

--- Project/Scripts/Install-VisualStudio-Windows.ps1
+++ Project/Scripts/Install-VisualStudio-Windows.ps1
@@ -4,6 +4,10 @@ param(
     [ValidateSet('BuildTools', 'Enterprise', 'Professional')]
     [string]$Edition,
     [Parameter(Mandatory = $false)]
+    [string]$Nickname = '',
+    [Parameter(Mandatory = $false)]
+    [string]$ProductKey = '',
+    [Parameter(Mandatory = $false)]
     [string]$TemporaryPath = '',
     [Parameter(Mandatory = $false)]
     [string]$Version = '17.2.3'
@@ -14,6 +18,10 @@ $localDirectoryPath = Join-Path `
     -Path (Get-PSDrive -Name 'Temp').Root;
 $visualStudioInstallerName = ('vs_{0}_{1}' -f $Edition, $Version)
 
+if ([string]::IsNullOrEmpty($Nickname)) {
+    $Nickname = ('BYTRC{0}{1}' -f $Version.Split('.')[0], $Edition[0]);
+}
+
 if (-not([string]::IsNullOrEmpty($TemporaryPath))) {
     $localDirectoryPath = $TemporaryPath;
 };
@@ -22,38 +30,14 @@ Expand-Archive `
     -DestinationPath $localDirectoryPath `
     -Path ('./{0}.zip' -f $visualStudioInstallerName);
 
-$configurationFilePath = (
-    Join-Path `
-        -AdditionalChildPath ('Configuration.json' -f $visualStudioInstallerName) `
-        -ChildPath $visualStudioInstallerName `
-        -Path $localDirectoryPath
-);
-$configurationFileValue = ConvertFrom-Json `
-    -InputObject (
-        Get-Content `
-            -Path (
-                Join-Path `
-                    -AdditionalChildPath ('Layout.json' -f $visualStudioInstallerName) `
-                    -ChildPath $visualStudioInstallerName `
-                    -Path $localDirectoryPath
-            ) `
-            -Raw
-    );
-
-New-Item `
-    -ItemType 'File' `
-    -Path $configurationFilePath `
-    -Value (
-        ConvertTo-Json -InputObject @{
-            components = $configurationFileValue.add;
-            version = '1.0';
-        }
-    ) |
-    Out-Null;
-
 $installerArguments = @(
-    '--config'
-    $configurationFilePath,
+    '--in'
+    (Join-Path `
+        -AdditionalChildPath ('Response.json' -f $visualStudioInstallerName) `
+        -ChildPath $visualStudioInstallerName `
+        -Path $localDirectoryPath),
+    '--nickname'
+    $Nickname,
     '--norestart',
     '--noWeb',
     '--quiet'
@@ -65,5 +49,9 @@ $installerCommand = Get-Command `
             -ChildPath $visualStudioInstallerName `
             -Path $localDirectoryPath
         );
+
+if (-not [string]::IsNullOrEmpty($ProductKey)) {
+    $installerArguments += ('--productKey', $ProductKey);
+}
 
 & $installerCommand $installerArguments;
