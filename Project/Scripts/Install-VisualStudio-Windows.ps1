5f1a65fd89f0a1f486f8be699005494b51dbb380
David Smith <kittoes0124@byteterrace.com>
2022-06-09 08:47:32 -0500

    added basic error handling and debugging information

--- Project/Scripts/Install-VisualStudio-Windows.ps1
+++ Project/Scripts/Install-VisualStudio-Windows.ps1
@@ -13,45 +13,69 @@ param(
     [string]$Version = '17.2.3'
 );
 
-$localDirectoryPath = Join-Path `
-    -ChildPath 'bytrc' `
-    -Path (Get-PSDrive -Name 'Temp').Root;
-$visualStudioInstallerName = ('vs_{0}_{1}' -f $Edition, $Version)
+$localDirectoryPath = $TemporaryPath;
 
-if ([string]::IsNullOrEmpty($Nickname)) {
+if ([string]::Empty -eq $Nickname) {
     $Nickname = ('BYTRC{0}{1}' -f $Version.Split('.')[0], $Edition[0]);
 }
 
-if (-not([string]::IsNullOrEmpty($TemporaryPath))) {
-    $localDirectoryPath = $TemporaryPath;
-};
+if ([string]::IsNullOrEmpty($TemporaryPath)) {
+    $localDirectoryPath = Join-Path `
+        -ChildPath 'bytrc' `
+        -Path (Get-PSDrive -Name 'Temp').Root;
+}
+
+$visualStudioInstallerName = ('vs_{0}_{1}' -f $Edition, $Version);
+$visualStudioInstallerPath = Join-Path `
+    -ChildPath $visualStudioInstallerName `
+    -Path $localDirectoryPath;
+
+Write-Debug 'Extracting Visual Studio Build Tools installer...';
 
 Expand-Archive `
     -DestinationPath $localDirectoryPath `
-    -Path ('./{0}.zip' -f $visualStudioInstallerName);
+    -Path (Join-Path `
+        -ChildPath ('{0}.zip' -f $visualStudioInstallerName) `
+        -Path (Get-Location));
 
 $installerArguments = @(
     '--in'
     (Join-Path `
-        -AdditionalChildPath ('Response.json' -f $visualStudioInstallerName) `
-        -ChildPath $visualStudioInstallerName `
-        -Path $localDirectoryPath),
-    '--nickname'
-    $Nickname,
+        -ChildPath ('Response.json' -f $visualStudioInstallerName) `
+        -Path $visualStudioInstallerPath),
     '--norestart',
     '--noWeb',
     '--quiet'
 );
 $installerCommand = Get-Command `
-    -Name (
-        Join-Path `
-            -AdditionalChildPath ('{0}.exe' -f $visualStudioInstallerName) `
-            -ChildPath $visualStudioInstallerName `
-            -Path $localDirectoryPath
-        );
+    -Name (Join-Path `
+        -ChildPath ('{0}.exe' -f $visualStudioInstallerName) `
+        -Path $visualStudioInstallerPath);
+
+if ($null -ne $Nickname) {
+    $installerArguments += ('--nickname', $Nickname);
+}
 
 if (-not [string]::IsNullOrEmpty($ProductKey)) {
     $installerArguments += ('--productKey', $ProductKey);
 }
 
-& $installerCommand $installerArguments;
+Write-Debug 'Running Visual Studio Build Tools installer...';
+
+$installerProcess = Start-Process `
+    -ArgumentList $installerArguments `
+    -FilePath $installerCommand `
+    -PassThru `
+    -Wait;
+$installerExitCode = $installerProcess.ExitCode;
+
+Write-Debug 'Removing Visual Studio Build Tools installer...';
+
+Remove-Item `
+    -Force `
+    -Path $visualStudioInstallerPath `
+    -Recurse;
+
+if ((0 -ne $installerExitCode) -and (3010 -ne $installerExitCode)) {
+    throw ('Visual Studio installer exited with failure code: {0}.' -f $installerExitCode);
+}
